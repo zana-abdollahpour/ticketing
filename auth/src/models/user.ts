b06cea6f44eed7b8b96739882151c74e033d591a
Zana Abdollahpour <zana.ap4@gmail.com>
2026-09-22 14:38:50 +0330

    fix ts error when transforming user to json

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -34,7 +34,7 @@ const userSchema = new mongoose.Schema(
   },
   {
     toJSON: {
-      transform(doc, ret) {
+      transform(doc, ret: any) {
         ret.id = ret._id;
         delete ret._id;
         delete ret.password;
